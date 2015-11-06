909460ae489abb8a409b2b82b4e3aac201b9b9fb
Travis <tkeating@ggc.edu>
2015-11-06 10:25:01 -0500

    TRAVIS -- Enemy stuff
    Created the constructor for the enemy, inherits from Persona. It will
    also implement the combat interface whenever I can get that to work.
    Contains getters and setters for the various attributes of enemy.

--- src/obstacles/Enemy.java
+++ src/obstacles/Enemy.java
@@ -12,9 +12,82 @@ public class Enemy {
 	//TODO: Write this class. Stubbed for inclusion into the controllers.GameController class.
 	//Need Enemy constructor that takes in String of planetName.
 	public Enemy(String originPlanet) {
+	
+	}
+	
+	//Declare enemy specific Attributes
+	private String enemyName;
+	private String enemyDescription;
+	private String enemyLocation;
+	private int ambushStatus;
 
+	/**
+	 * CONSTRUCTOR FOR ENEMIES with super()
+	 * @param enemyName
+	 * @param enemyDescription
+	 * @param enemyLocation
+	 * @param ambushStatus
+	 * 
+	 * CREATED: 11/3/15
+	 * LAST EDITED ON: 11/3/15  BY: Travis K.
+	 */
+	public Enemy(String enemyName, String enemyDescription, String enemyLocation, int ambushStatus) {
+		super();
+		
+		this.enemyName = enemyName;
+		this.enemyDescription = enemyDescription;
+		this.enemyLocation = enemyLocation;
+		this.ambushStatus = ambushStatus;
+	}
+
+	
+	
+	/**
+	 * @return the enemy name
+	 */
+	
+	public String getEnemyName() {
+		return enemyName;
+	}
+	
+	/**
+	 * @return the enemy description
+	 */
+	public String getEnemyDescription() {
+		return enemyDescription;
+	}
+	
+	/**
+	 * @return the enemy location
+	 */
+	public String getEnemyLocation() {
+		return enemyLocation;
+	}
+	
+	/**
+	 * @return the ambush status
+	 * 
+	 * Note to Self: 
+	 * 		When ambushStatus == 0 -> not an ambush
+	 * 		When ambushStatus == 1 -> is an ambush
+	 * 	I'm assuming this is what ambushStatus is supposed to be?
+	 * 		--Travis K.
+	 */
+	public int getAmbushStatus() {
+		return ambushStatus;
 	}
 
 
-
+////Implement Combat Interface
+//	public int attack(int attackPoints, int defensePoints) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//	
+    //private int[] enemyList;
+		/* I'm leaving enemyList out for now, I don't think it has much
+		 * use in this class, Let me know if you want here --Travis K*/
+	
+	
 }
+
